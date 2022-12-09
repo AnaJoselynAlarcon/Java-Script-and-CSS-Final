9f9a41294138f2585f3eaa2f3fdc0a5f649a3fb2
AnaJoselynAlarcon <ana.alarconfigueroa@edu.sait.ca>
2022-12-08 22:14:17 -0700

    Css and JS not finished

--- asn-js-PROVIDED/scripts/booking.js
+++ asn-js-PROVIDED/scripts/booking.js
@@ -1,34 +1,144 @@
 /********* create variables *********/
-// useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
+// useful variables might be: the cost per day, the number of days selected, 
+// and elements on the screen that will be clicked or will need to be modified. 
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
+var cost_per_day = 35;
+var number_of_days = 0;
+
 
 
 
 
 /********* colour change days of week *********/
-// when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
-// added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
+// when the day buttons are clicked, 
+//we will apply the "clicked" class to that element,
+// and update any other relevant variables. 
+// Then, we can recalculate the total cost.
+
+// added challenge: don't update the dayCounter if the same day is clicked more than once.
+// hint: .classList.contains() might be helpful here!
+var monday = document.getElementById("monday");
+var tuesday = document.getElementById("tuesday");
+var wednesday = document.getElementById("wednesday");
+var thursday = document.getElementById("thursday");
+var friday = document.getElementById("friday");
 
 
+//all days
+//full day and half day
+var fullDay = document.getElementById("full");
+var halfDay = document.getElementById("half");
 
+
+function ColourChange()
+{
+    if(this.classList.contains("clicked"))
+    {this.classList.remove ("clicked");}
+
+    else 
+    {this.classList.add("clicked");
+    number_of_days = number_of_days +1
+    //number_of_days ++
+    }
+    calculate()
+
+}
+
+monday.addEventListener("click", ColourChange);
+tuesday.addEventListener("click", ColourChange);
+wednesday.addEventListener("click", ColourChange);
+thursday.addEventListener("click", ColourChange);
+friday.addEventListener("click", ColourChange);
+fullDay.addEventListener("click", ColourChange);
+halfDay.addEventListener("click", ColourChange);
+
+//click is a keyword
+//event listener for all of them
 
 
 /********* clear days *********/
-// when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
+// when the clear-button is clicked, the "clicked" class is removed from all days,
+// any other relevant variables are reset, and the calculated cost is set to 0.
+
+//number of days 0
+var clear = document.getElementById("clear-button");
 
 
+function clearDays()
+{
+    // var clear = document.getElementById("clear-button");
+    halfDay.classList.remove("clicked");
+    monday.classList.remove("clicked");
+    tuesday.classList.remove("clicked");
+    wednesday.classList.remove("clicked");
+    thursday.classList.remove("clicked");
+    friday.classList.remove("clicked");
+    document.getElementById("calculated-cost").innerHTML = 0;
+    number_of_days = 0;
+}
+
+//     if (this.classList.contains("clicked"))
+//         {this.classList.remove ("clicked");
+//         cost_per_day = 0;
+//         number_of_days = 0;}
+    
+//     calculate()
+// }
 
 
-
+clear.addEventListener("click", clearDays)
 
 /********* change rate *********/
-// when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
+// when the half-day button is clicked,
+// set the daily rate to $20, add the "clicked" class to the "half" element, 
+// remove it from the "full" element, and recalculate the total cost.
+var fullDay = document.getElementById("full");
+var halfDay = document.getElementById("half");
+
+function halfRate()
+{
+    if(halfDay == "half")
+    {
+        var fullDay = document.getElementById("full");
+        fullDay.classList.remove("clicked");
+        cost_per_day = 20;}
+    else
+    {
+        halfDay.classList.remove("clicked");
+        cost_per_day = 35;
+    }
+
+    // else 
+    // {this.classList.add("clicked");
+    // number_of_days = number_of_days +1
+    // //number_of_days ++
+    // }
+    calculate()
+
+}
+if(halfDay)
+{
+halfDay.addEventListener("click", halfRate);
+}
+
+
+
+//daily rate = costperday
+//add class list for the half
+//remove class full day
+//calculate function again 
 
 
 
 
-// when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
+
+
+
+// when the full-day button is clicked, 
+//the daily rate is set back to $35, 
+//the clicked class is added to "full"
+// and removed from "half", and the total cost is recalculated.
 
 
 
@@ -37,4 +147,17 @@
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 
+var cost = document.getElementById("calculated-cost");
 
+function calculate()
+{
+    var cost_text = document.getElementById("calculated-cost");
+        cost_text = cost_per_day * parseInt(number_of_days);
+        
+        document.getElementById("calculated-cost").innerHTML = cost_text;
+}
+
+if(cost)
+{
+    cost.addEventListener("click", calculate);
+}
--- asn-js-PROVIDED/scripts/contact.js
+++ asn-js-PROVIDED/scripts/contact.js
@@ -1,4 +1,18 @@
-// when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
+// when the "submit-button" is clicked, the contents of the 
+//contact-page are replaced with a single <p> element
+// that reads "Thank you for your message" in size 24 font.
 
-// hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
+// hint: you can change the style of an element
+// by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+// style.fontSize="24 px";
+var button = document.getElementById("submit-button");
+
+function GoodDogSumbit()
+{
+    var message = document.getElementById("contact-page");
+    message.innerHTML =  "Thank You";
+    message.style.fontSize="24px";
+}
+
+button.addEventListener("click", GoodDogSumbit)
